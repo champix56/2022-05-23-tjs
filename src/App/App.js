b13e3367cb8e209259e6061ab35881999dad4972
champix56 <desorbaix@free.fr>
2022-05-23 16:17:46 +0200

    gestion etatique sur class

--- src/App/App.js
+++ src/App/App.js
@@ -3,26 +3,30 @@ import "./App.css";
 import Button from "./components/Button/Button";
 
 class App extends React.Component {
-  counter = 0;
+  constructor(props){
+    super(props);
+    // valeur initial du conteneur etatique
+    this.state={counter:0,message:'aucun message'}
+  }
   render() {
     return (
       <div className="App" style={{ textAlign: "center" }}>
-        <div>Valeur de COUNTER : {this.counter}</div>
+        <div>Valeur de COUNTER : {this.state.counter}</div>
         <hr />
         <Button
           text="Ajouter 1"
           bgcolor="skyblue"
           onButtonClicked={() => {
-            this.counter++;
-            console.log(this.counter);
+            this.setState({counter:this.state.counter+1});
+            console.log(this.state.counter);
           }}
         />
         <Button
           text="Soustraire 1"
           bgcolor="tomato"
           onButtonClicked={() => {
-            this.counter--;
-            console.log(this.counter);
+            this.setState({counter:this.state.counter-1});
+            console.log(this.state.counter);
           }}
         />
       </div>
