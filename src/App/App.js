084d084d8b49d712099a8c420f3eee453528bf6e
champix56 <desorbaix@free.fr>
2022-05-23 15:36:29 +0200

    erreur sans etat

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,32 @@
 import React from "react";
 import "./App.css";
 import Button from "./components/Button/Button";
-function App() {
-  return (
-    <div className="App">
-      DEMAT BREIZH
-      <br />
-      <Button
-        text="Hello"
-        onButtonClicked={() => {
-          console.log();
-        }}
-        bgcolor="tomato"
-        color="green"
-      />
-      <Button text="Button value" styleDuButton={{textDecoration:'underline blue', fontStyle:'italic'}} />
-      <Button text="Click ici" />
-      <Button />
-    </div>
-  );
-}
 
+class App extends React.Component {
+  counter = 0;
+  render() {
+    return (
+      <div className="App" style={{ textAlign: "center" }}>
+        <div>Valeur de COUNTER : {this.counter}</div>
+        <hr />
+        <Button
+          text="Ajouter 1"
+          bgcolor="skyblue"
+          onButtonClicked={() => {
+            this.counter++;
+            console.log(this.counter);
+          }}
+        />
+        <Button
+          text="Soustraire 1"
+          bgcolor="tomato"
+          onButtonClicked={() => {
+            this.counter--;
+            console.log(this.counter);
+          }}
+        />
+      </div>
+    );
+  }
+}
 export default App;
