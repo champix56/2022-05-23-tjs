de1db40379a93afc79f6617e93245660842a30c9
champix56 <desorbaix@free.fr>
2022-05-23 12:28:59 +0200

    creation d'un composant ui function

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
-
+import Button from './components/Button/Button'
 function App() {
   return (
     <div className="App">
-     DEMAT BREIZH
+     DEMAT BREIZH<br/>
+     <Button/>
+     <Button/>
+     <Button/>
+     <Button/>
+     <Button/>
+     <Button/>
     </div>
   );
 }
---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// contenu du cmp
+function Button(props) {
+    return <button>Benjamin</button>;
+}
+
+//exportation 
+export default Button;
