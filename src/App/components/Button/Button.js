983c9cd3034447da8a1c0cdc71af21c912afc7ee
champix56 <desorbaix@free.fr>
2022-05-23 15:14:35 +0200

    proptypes

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,22 +1,30 @@
 import React from "react";
 import style from './Button.module.css'
+import PropTypes from 'prop-types';
 // contenu du cmp
 function Button(props) {
   console.warn(props,style);
   return (
     <button className={style.Button}
       onClick={(evt) => {
+        if(undefined !== props.onButtonClicked && typeof props.onButtonClicked === 'function')
         props.onButtonClicked()
       }} 
       style={{
           backgroundColor:props.bgcolor,
           color:props.color,
       }}
+      type={props.type}
     >
       {props.text}
     </button>
   );
 }
-
+Button.propTypes={
+    bgcolor: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    onButtonClicked: PropTypes.func,
+    type: PropTypes.string,
+}
 //exportation
 export default Button;
