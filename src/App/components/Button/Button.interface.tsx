224b88086d89a2f8290812335224a6e60d7fb223
champix56 <desorbaix@free.fr>
2022-05-24 11:52:24 +0200

    facultatif de champs pour parents ts

--- src/App/components/Button/Button.interface.tsx
+++ src/App/components/Button/Button.interface.tsx
@@ -1,8 +1,8 @@
 import { ReactNode } from 'react';
 //def. du format des props
 export interface IButtonProps {
-  bgcolor: string;
-  color: string;
+  bgcolor?: string;
+  color?: string;
   type?: 'button' | 'reset' | 'submit';
   styleDuButton?: object;
   onButtonClicked?: Function;
