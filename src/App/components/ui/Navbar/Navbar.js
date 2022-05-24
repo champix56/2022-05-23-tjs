904df49c9897fb5ec596eba1e8a943a8e6427c70
champix56 <desorbaix@free.fr>
2022-05-24 11:24:53 +0200

    ajout bootstrap pour navbar

--- src/App/components/ui/Navbar/Navbar.js
+++ src/App/components/ui/Navbar/Navbar.js
@@ -1,22 +1,24 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import style from "./Navbar.module.css";
+import { Navbar as NavBar, Container, Nav } from 'react-bootstrap';
 
-const initialState = {};
 
 const Navbar = (props) => {
-  const [state, setstate] = useState(initialState);
-  //effet mount/update
-  useEffect(() => {}, [props]);
   return (
     <div className={style.Navbar} data-testid="Navbar">
-      navbar
+      <NavBar bg="dark" variant="dark">
+        <Container>
+          <NavBar.Brand href="#home">Navbar</NavBar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </NavBar>
     </div>
   );
 };
-
-Navbar.propTypes = {};
-
-Navbar.defaultProps = {};
 
 export default Navbar;
