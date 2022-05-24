c77f696f07a6e8172a9e4f72bc7dc6ca904afd2a
champix56 <desorbaix@free.fr>
2022-05-24 11:48:56 +0200

    fix button type for type interface

--- src/App/components/Button/Button.interface.tsx
+++ src/App/components/Button/Button.interface.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 export interface IButtonProps {
   bgcolor: string;
   color: string;
-  type?: "button" | "reset" | "submit";
+  type?: 'button' | 'reset' | 'submit';
   styleDuButton?: object;
   onButtonClicked?: Function;
   children: Array<ReactNode | string> | string | ReactNode;
