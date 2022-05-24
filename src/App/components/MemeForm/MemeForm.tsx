580da17b082ffecbf635db2e94a7e0481d8e8fb8
champix56 <desorbaix@free.fr>
2022-05-24 11:43:31 +0200

    css memeform+reformat js

--- src/App/components/MemeForm/MemeForm.tsx
+++ src/App/components/MemeForm/MemeForm.tsx
@@ -8,105 +8,66 @@ interface IMemeFormState {}
 const initialState: IMemeFormState = {};
 const MemeForm: React.FC<IMemeFormProps> = (props) => {
   return (
-       <div data-testid="MemeForm" className={style.MemeForm}>
-        <form >
-          <h1>Titre</h1>
-          <input
-            type="text"
-            id="f_titre"
-            placeholder="saisir titre"
-             />
-          <hr />
-          <h2>Image</h2>
-          <select
-
-          >
-            <option value="-1">Aucune</option>
-
-          </select>
-          <hr />
-          <h2>text</h2>
-          <input
-            type="text"
-
-          />
-          <div className={styles.half}>
-            <div>
-              <label htmlFor="f_x">x:</label>
-              <br />
-              <input
-                type="number"
-                className={styles.smallInput}
-
-              />
-            </div>
-            <div>
-              <label htmlFor="f_y">y:</label>
-              <br />
-              <input
-                type="number"
-                className={styles.smallInput}
-
-              />
-            </div>
+    <div data-testid="MemeForm" className={style.MemeForm}>
+      <form>
+        <h1>Titre</h1>
+        <input type="text" id="f_titre" placeholder="saisir titre" />
+        <hr />
+        <h2>Image</h2>
+        <select>
+          <option value="-1">Aucune</option>
+        </select>
+        <hr />
+        <h2>text</h2>
+        <input type="text" />
+        <div className={style.half}>
+          <div>
+            <label htmlFor="f_x">x:</label>
+            <br />
+            <input type="number" className={style.smallInput} />
           </div>
-          <hr />
-          <label htmlFor="f_color">Couleur</label>
-          <input
-            type="color"
-            id="f_color"
-
-          />
-          <div className={styles.half}>
-            <div>
-              <label htmlFor="f_sz">font-size:</label>
-              <br />
-              <input
-                type="number"
-                className={styles.smallInput}
-
-                min={0}
-
-              />
-            </div>
-            <div>
-              <label htmlFor="f_fw">font-weight:</label>
-              <br />
-              <input
-                type="number"
-                className={styles.smallInput}
-                min="100"
-                step="100"
-                max="900"
-
-              />
-            </div>
+          <div>
+            <label htmlFor="f_y">y:</label>
+            <br />
+            <input type="number" className={style.smallInput} />
           </div>
-          <div className={styles.half}>
-            <div>
-              <label htmlFor="f_underline">underline:</label>
-              <br />
-              <input
-                id="f_underline"
-                type="checkbox"
-
-              />
-            </div>
-            <div>
-              <label htmlFor="f_italic">italic:</label>
-              <br />
-              <input
-                id="f_italic"
-                type="checkbox"
-
-              />
-            </div>
+        </div>
+        <hr />
+        <label htmlFor="f_color">Couleur</label>
+        <input type="color" id="f_color" />
+        <div className={style.half}>
+          <div>
+            <label htmlFor="f_sz">font-size:</label>
+            <br />
+            <input type="number" className={style.smallInput} min={0} />
           </div>
-          <div className={styles.half}>
-
+          <div>
+            <label htmlFor="f_fw">font-weight:</label>
+            <br />
+            <input
+              type="number"
+              className={style.smallInput}
+              min="100"
+              step="100"
+              max="900"
+            />
           </div>
-        </form>
-      </div>
+        </div>
+        <div className={style.half}>
+          <div>
+            <label htmlFor="f_underline">underline:</label>
+            <br />
+            <input id="f_underline" type="checkbox" />
+          </div>
+          <div>
+            <label htmlFor="f_italic">italic:</label>
+            <br />
+            <input id="f_italic" type="checkbox" />
+          </div>
+        </div>
+        <div className={style.half}></div>
+      </form>
+    </div>
   );
 };
 export default MemeForm;
