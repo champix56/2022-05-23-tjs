e89d9f588ff33456be7aeb76949bc6f7b32547a4
champix56 <desorbaix@free.fr>
2022-05-25 11:47:23 +0200

    connexion du cp

--- src/App/components/MemeThumbnail/MemeThumbnail.tsx
+++ src/App/components/MemeThumbnail/MemeThumbnail.tsx
@@ -1,6 +1,8 @@
 import { MemeSVGViewer } from "orsys-tjs-meme";
 import { IImage, IMeme } from "orsys-tjs-meme/dist/interfaces/common";
 import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { IStoreState } from "../../store/store";
 import style from "./MemeThumbnail.module.css";
 
 //types
@@ -11,15 +13,24 @@ interface IMemeThumbnailProps {
 
 //component
 const MemeThumbnail: React.FC<IMemeThumbnailProps> = (props) => {
+  console.log(props)
   return (
     <div className={style.MemeThumbnail} data-testid="MemeThumbnail">
-      {props.memes.map((meme: IMeme,i:number) => (
-        <div className={style.vignette} key={'thumb'+i}>
-          <MemeSVGViewer meme={meme} image={props.images.find(img=>img.id===meme.imageId)} />
+      {props.memes.map((meme: IMeme, i: number) => (
+        <div className={style.vignette} key={"thumb" + i}>
+          <MemeSVGViewer
+            meme={meme}
+            image={props.images.find((img) => img.id === meme.imageId)}
+          />
         </div>
       ))}
     </div>
   );
 };
-
-export default MemeThumbnail;
+function mstp(s: IStoreState, p: any) {
+  return { ...p, ...s.ressources };
+}
+function mdtp() {
+  return {};
+}
+export default connect(mstp, mdtp)(MemeThumbnail);
