b08f80df6d1351103dc1bea1e6281a4af37bdc13
champix56 <desorbaix@free.fr>
2022-05-25 15:13:49 +0200

    add router links

--- src/App/components/ui/Navbar/Navbar.js
+++ src/App/components/ui/Navbar/Navbar.js
@@ -3,17 +3,28 @@ import PropTypes from "prop-types";
 import style from "./Navbar.module.css";
 import { Navbar as NavBar, Container, Nav } from 'react-bootstrap';
 import '../../../../../node_modules/bootstrap/dist/css/bootstrap.css'
-
+import { LinkContainer } from 'react-router-bootstrap'
 const Navbar = (props) => {
   return (
     <div className={style.Navbar} data-testid="Navbar">
       <NavBar bg="dark" variant="dark">
         <Container>
-          <NavBar.Brand href="#home">Navbar</NavBar.Brand>
+          <LinkContainer to="/">
+            <NavBar.Brand >Navbar</NavBar.Brand>
+          </LinkContainer>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/thumbnail">
+              <Nav.Link >Thumbnail</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/editor">
+              <Nav.Link>New</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/editor/0">
+              <Nav.Link>Edit id:0</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Container>
       </NavBar>
