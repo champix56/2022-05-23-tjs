6fe353e685a8690712d74b6f45c3bbec2caa0abb
champix56 <desorbaix@free.fr>
2022-05-25 10:43:13 +0200

    store combiné

--- src/App/store/store.tsx
+++ src/App/store/store.tsx
@@ -1,5 +1,5 @@
-import { IMeme } from "orsys-tjs-meme/dist/interfaces/common";
-import { createStore } from "redux";
+import { IImage, IMeme } from "orsys-tjs-meme/dist/interfaces/common";
+import { combineReducers, createStore } from "redux";
 
 export const emptyMeme: IMeme = {
   color: "#000000",
@@ -21,8 +21,8 @@ export const ACTIONS_CURRENT = Object.freeze({
 function currentReducer(
   state: IMeme = emptyMeme,
   action: { type: string; value?: object }
-):IMeme {
-    console.log(action.type)
+): IMeme {
+  console.log(action.type);
   switch (action.type) {
     case ACTIONS_CURRENT.CLEAR_MEME:
       return { ...emptyMeme };
@@ -34,12 +34,47 @@ function currentReducer(
       return state;
   }
 }
-export const store=createStore(currentReducer,
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-    );
-store.subscribe(()=>{
-    console.trace(store.getState())
-})
+
+const ressourcesInitialState = {
+  memes: [],
+  images: [],
+};
+
+const ressourcesReducer = (
+  state = ressourcesInitialState,
+  action: {
+    type: string;
+    value?: IMeme | IImage;
+    values?: Array<IMeme | IImage>;
+  }
+) => {
+  switch (action.type) {
+    case "ADD_MEME":
+      return { ...state, memes: [...state.memes, action.value] };
+    default:
+      return state;
+  }
+};
+export interface IStoreState {
+  ressources: {
+    images: Array<IImage>;
+    memes: Array<IMeme>;
+  };
+  current: IMeme;
+}
+
+const combinedReducers = combineReducers({
+  ressources: ressourcesReducer,
+  current: currentReducer,
+});
+export const store = createStore(
+  combinedReducers,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
+store.subscribe(() => {
+  console.trace(store.getState());
+});
 /*
 store.dispatch({type:ACTIONS_CURRENT.UPDATE_MEME,value:{italic:true,underline:true},})
 store.dispatch({type:ACTIONS_CURRENT.UPDATE_MEME,value:{text:'hello de lu'},})
