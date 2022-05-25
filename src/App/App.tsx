20c365db79df03c21cf83514adba3158229dc216
champix56 <desorbaix@free.fr>
2022-05-25 11:47:36 +0200

    clean no sate app

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,81 +1,25 @@
 import React from "react";
 import "./App.css";
-import { MemeSVGViewer } from "orsys-tjs-meme";
 import FlexWide from "./components/layout/FlexWide/FlexWide";
-import MemeForm, { ConnectedMemeForm } from "./components/MemeForm/MemeForm";
 import Header from "./components/ui/Header/Header";
 import Navbar from "./components/ui/Navbar/Navbar";
 import Footer from "./components/ui/Footer/Footer";
-import { IImage, IMeme } from "orsys-tjs-meme/dist/interfaces/common";
-import EditorWrapper from "./components/EditorWrapper/EditorWrapper";
-import { ADR_REST, REST_RESSOURCES } from "./config/config";
 import MemeThumbnail from "./components/MemeThumbnail/MemeThumbnail";
-import {ACTIONS_CURRENT} from './store/store'
-interface IAppState {
-  meme: IMeme;
-  memes: Array<IMeme>;
-  images: Array<IImage>;
-}
-class App extends React.Component<{}, IAppState> {
-  constructor(props: {}) {
-    super(props);
-    console.log(ACTIONS_CURRENT)
-    // valeur initial du conteneur etatique
-    this.state = {
-      meme: {
-        color: "#EC0BAE",
-        fontSize: 40,
-        fontWeight: "900",
-        imageId: 0,
-        italic: false,
-        text: "1er meme",
-        titre: "dummy",
-        underline: false,
-        x: 0,
-        y: 50,
-      },
-      memes: [],
-      images: [],
-    };
-  }
-  componentDidMount() {
-    const pimg=fetch(`${ADR_REST}${REST_RESSOURCES.images}`, {
-      method: "GET",
-      headers: { Accept: "application/json" },
-    }).then((f) => f.json());
-    const pmeme=fetch(`${ADR_REST}${REST_RESSOURCES.memes}`, {
-      method: "GET",
-      headers: { Accept: "application/json" },
-    })
-      .then((f) => f.json())
+import ConnectedMemeSvgViewer from "./components/ConnectedMemeSvgViewer/ConnectedMemeSvgViewer";
+import { ConnectedMemeForm } from "./components/MemeForm/MemeForm";
 
-    //const tm=new Promise(()=>{setTimeout(()=>{},100)})
-    //const prAll= 
-    Promise.all([pimg,pmeme]).then(arr_arr=>{
-      this.setState({ images: arr_arr[0],memes:arr_arr[1] });
-     })
-     /*Promise.race([tm,prAll]).then(f=>{
-       console.log(f);
-     })*/
-  }
+class App extends React.Component<undefined, undefined> {
   render() {
     return (
       <div className="App" style={{ textAlign: "center" }}>
         <Header />
         <Navbar />
-        <MemeThumbnail memes={this.state.memes} images={this.state.images} />
+        <MemeThumbnail />
         <FlexWide>
-          <MemeSVGViewer
-            meme={this.state.meme}
-            image={this.state.images.find(
-              (img: IImage) => img.id === this.state.meme.imageId
-            )}
-          />
-          <ConnectedMemeForm
-            images={this.state.images}
-          />
+          <ConnectedMemeSvgViewer/>
+          <ConnectedMemeForm/>
         </FlexWide>
-        <Footer>{JSON.stringify(this.state.meme)}</Footer>
+        <Footer>Desorbaix &amp; Orsys &copy; 2022</Footer>
       </div>
     );
   }
