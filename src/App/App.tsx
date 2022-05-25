c075399cd21dba87508e455c5627dd9f5e6125a3
champix56 <desorbaix@free.fr>
2022-05-25 15:02:29 +0200

    manip de la route

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -7,7 +7,8 @@ import Footer from "./components/ui/Footer/Footer";
 import MemeThumbnail from "./components/MemeThumbnail/MemeThumbnail";
 import ConnectedMemeSvgViewer from "./components/ConnectedMemeSvgViewer/ConnectedMemeSvgViewer";
 import { ConnectedMemeForm } from "./components/MemeForm/MemeForm";
-import { ConnectedModal, Modal } from "./components/Modal/Modal";
+import { ConnectedModal } from "./components/Modal/Modal";
+import { Link, Route, Switch } from "react-router-dom";
 
 class App extends React.Component<{}, {}> {
   render() {
@@ -16,11 +17,28 @@ class App extends React.Component<{}, {}> {
         <div className="App" style={{ textAlign: "center" }}>
           <Header />
           <Navbar />
-          <MemeThumbnail />
-          <FlexWide>
-            <ConnectedMemeSvgViewer />
-            <ConnectedMemeForm />
-          </FlexWide>
+          <div>
+            <Link to="/">home</Link>&nbsp;<Link to="/editor">editeur</Link>
+            &nbsp;<Link to="/thumbnail">thumbnail</Link>&nbsp;
+          </div>
+          <Switch>
+            <Route path="/" exact>
+              <h1>Bienvenu sur l'app</h1>
+            </Route>
+            <Route path="/editor">
+              <FlexWide>
+                <ConnectedMemeSvgViewer />
+                <ConnectedMemeForm />
+              </FlexWide>
+            </Route>
+            <Route path="/thumbnail">
+              <MemeThumbnail />
+            </Route>
+            <Route path="/">
+              <h1>Route non disponible</h1>
+            </Route>
+          </Switch>
+
           <Footer>Desorbaix &amp; Orsys &copy; 2022</Footer>
         </div>
         <ConnectedModal />
