5b9eab966f3d2d4495b4633da65f9a25e912782a
champix56 <desorbaix@free.fr>
2022-05-25 15:54:59 +0200

    gestion de selection du courrant par route params

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import FlexWide from "./components/layout/FlexWide/FlexWide";
 import Header from "./components/ui/Header/Header";
@@ -8,7 +8,10 @@ import MemeThumbnail from "./components/MemeThumbnail/MemeThumbnail";
 import ConnectedMemeSvgViewer from "./components/ConnectedMemeSvgViewer/ConnectedMemeSvgViewer";
 import { ConnectedMemeForm } from "./components/MemeForm/MemeForm";
 import { ConnectedModal } from "./components/Modal/Modal";
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch, withRouter } from "react-router-dom";
+import { emptyMeme, IStoreState, store } from "./store/store";
+import { connect } from "react-redux";
+import { IMeme } from "orsys-tjs-meme/dist/interfaces/common";
 
 class App extends React.Component<{}, {}> {
   render() {
@@ -25,11 +28,11 @@ class App extends React.Component<{}, {}> {
             <Route path="/" exact>
               <h1>Bienvenu sur l'app</h1>
             </Route>
-            <Route path="/editor">
-              <FlexWide>
-                <ConnectedMemeSvgViewer />
-                <ConnectedMemeForm />
-              </FlexWide>
+            <Route path="/editor" exact>
+              <EditorConstructionWithRouter />
+            </Route>
+            <Route path="/editor/:id">
+              <EditorConstructionWithRouter />
             </Route>
             <Route path="/thumbnail">
               <MemeThumbnail />
@@ -47,3 +50,30 @@ class App extends React.Component<{}, {}> {
   }
 }
 export default App;
+const EditorConstruction: React.FC<any> = (props) => {
+  console.log(props);
+  useEffect(() => {
+    const foundMeme = props.memes.find(
+      (m: IMeme) => m.id === Number(props.match.params?.id)
+    );
+   props.selectMeme(foundMeme);
+  }, [props]);
+  return (
+    <FlexWide>
+      <ConnectedMemeSvgViewer />
+      <ConnectedMemeForm />
+    </FlexWide>
+  );
+};
+function mstp(s: IStoreState, p: any) {
+  return { ...p, memes: s.ressources.memes };
+}
+function mdtp(d: Function) {
+  return {
+    selectmeme: (meme: IMeme|undefined) =>
+      d({ type: "UPDATE_MEME", value: meme ? meme : emptyMeme }),
+  };
+}
+const EditorConstructionWithRouter = withRouter(
+  connect(mstp, mdtp)(EditorConstruction)
+);
